0023956dd5cf7e8741e02497b2405482183764fc
Víctor Briseño <victor.briseno.leon@hotmail.com>
2026-05-13 08:12:29 +0000

    Mover a un solo archivo

--- api/dashboad.go
+++ api/dashboad.go
@@ -14,50 +14,50 @@ import (
 
 func TotalsHandler(mongoClient *mongo.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		datos, err := ObtenerTotales(mongoClient)
+		var totales struct {
+			TotalCredito      float64 `json:"totalCredito"`
+			TotalDisponible   float64 `json:"totalDisponible"`
+			TotalAhorro       float64 `json:"totalAhorro"`
+			TotalApalancado   float64 `json:"totalApalancado"`
+			TotalMsi          float64 `json:"totalMsi"`
+			TotalUsado        float64 `json:"totalUsado"`
+			UtilizacionGlobal float64 `json:"utilizacionGlobal"`
+		}
+
+		collection := mongoClient.Database(dbName).Collection(collectionName)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		cursor, err := collection.Find(ctx, bson.D{})
 		if err != nil {
 			log.Printf("Error obteniendo totales: %v", err)
 			http.Error(w, "Error interno del servidor", http.StatusInternalServerError)
 			return
 		}
+		defer cursor.Close(ctx)
+
+		var tarjetas []modelos.Tarjeta
+		if err = cursor.All(ctx, &tarjetas); err != nil {
+			log.Printf("Error obteniendo totales: %v", err)
+			http.Error(w, "Error interno del servidor", http.StatusInternalServerError)
+			return
+		}
+
+		for i := range tarjetas {
+			tarjetas[i].CalcularCredito()
+			totales.TotalCredito += tarjetas[i].Credito
+			totales.TotalDisponible += tarjetas[i].Disponible
+			totales.TotalAhorro += tarjetas[i].Tener
+			totales.TotalApalancado += tarjetas[i].Apalancamiento
+			totales.TotalMsi += tarjetas[i].Msi
+		}
+
+		totales.TotalUsado = totales.TotalCredito - totales.TotalDisponible
+		if totales.TotalCredito > 0 {
+			totales.UtilizacionGlobal = (totales.TotalUsado / totales.TotalCredito) * 100
+		}
 
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(datos)
+		json.NewEncoder(w).Encode(totales)
 	}
-}
-
-func ObtenerTotales(mongoClient *mongo.Client) (modelos.Totales, error) {
-	var totales modelos.Totales
-
-	collection := mongoClient.Database(dbName).Collection(collectionName)
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	cursor, err := collection.Find(ctx, bson.D{})
-	if err != nil {
-		return totales, err
-	}
-	defer cursor.Close(ctx)
-
-	var tarjetas []modelos.Tarjeta
-	if err = cursor.All(ctx, &tarjetas); err != nil {
-		return totales, err
-	}
-
-	for i := range tarjetas {
-		tarjetas[i].CalcularCredito()
-
-		totales.TotalCredito += tarjetas[i].Credito
-		totales.TotalDisponible += tarjetas[i].Disponible
-		totales.TotalAhorro += tarjetas[i].Tener
-		totales.TotalApalancado += tarjetas[i].Apalancamiento
-		totales.TotalMsi += tarjetas[i].Msi
-	}
-
-	totales.TotalUsado = totales.TotalCredito - totales.TotalDisponible
-	if totales.TotalCredito > 0 {
-		totales.UtilizacionGlobal = (totales.TotalUsado / totales.TotalCredito) * 100
-	}
-
-	return totales, nil
 }
